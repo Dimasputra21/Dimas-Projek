53370f85e39e53f36cd1a28adcd4de88cdb296a0
Dimasputra21 <dimasputra21@student.ub.ac.id>
2023-01-04 14:33:45 +0700

    Commit Projek Katalog Buku
    step 1 (file admin, dist, img, dan imgbook)

---
+++ Projek Katalog-Buku/admin/include/konfirmasieditpenerbit.php
@@ -0,0 +1,16 @@
+<?php
+    if (isset($_SESSION['id_penerbit'])) {
+        $id_penerbit = $_SESSION['id_penerbit'];
+        $penerbit = $_POST['penerbit'];
+        $alamat = $_POST['alamat'];
+        if (empty($penerbit && $alamat)) {
+            header("Location:index.php?include=edit-penerbit&data=".$id_penerbit."&notif=editkosong");
+        }else {
+            $sql = "update penerbit set penerbit='$penerbit' ,  alamat='$alamat' 
+            WHERE id_penerbit = '$id_penerbit'";
+            mysqli_query($koneksi,$sql);
+            unset($_SESSION['id_penerbit']);
+            header("Location:index.php?include=penerbit&notif=editberhasil");
+        }
+    }
+?>
